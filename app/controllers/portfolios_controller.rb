3adf3ae278ffdbe8dcf64db78204d18735523386
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-10-13 16:03:33 -0600

    Integrated new and create actions for portfolios'

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,8 @@
 class PortfoliosController < ApplicationController
 	def index
-		@portfolio_items = Portfolio.all
+		@portfolio_item = Portfolio.all
 	end
-
-
+	def new
+		@portfolio_item = Portfolio.new
+	end
 end
