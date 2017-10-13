a2b9e99286adbc8ab67e9fba96316bd518ad221e
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-10-13 13:47:55 -0600

    integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,8 +33,8 @@ puts "5 skills created"
     incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam 
     corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate 
     velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-		main_images: "http://via.placeholder.com/600x400",
-		thumb_images: "http://via.placeholder.com/350x200"
+		main_image: "http://via.placeholder.com/600x400",
+		thumb_image: "http://via.placeholder.com/350x200"
 		)
 end
 
