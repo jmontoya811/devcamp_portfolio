9665c49f14dfc4304751485ca0fa87a4e8be4e3c
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-10-20 15:41:23 -0600

    integrated guest user function

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,13 @@
+module CurrentUserConcern
+
+	def current_user
+  		super || guest_user
+  	end
+
+  	def guest_user
+  		OpenStruct.new(name: "Guest User",
+  						 first_name: "Guest",
+  		 				last_name: "User",
+  						 email: "a@b.com")
+  	end
+end
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,7 +13,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -48,7 +48,7 @@ class PortfoliosController < ApplicationController
   def update
     @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The portfolio item was successfully updated.' }
         format.json { render :show, status: :ok, location: @portfolio_item }
       else
@@ -62,4 +62,12 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
 
+    private
+
+    def portfolio_params
+      params.require(:portfolio).permit(:title, 
+                                        :subtitle,
+                                        :body,
+                                        technologies_attributes: [:name])
+    end
 end
