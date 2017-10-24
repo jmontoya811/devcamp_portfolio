6d4af6e1900da88dadb8a1a57fe0adbad9ab326a
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-10-24 15:05:32 -0600

    integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,8 @@ module ApplicationHelper
 		content_tag(:p, greeting, class: "source-greeting")
 	end
 end
+
+def copyright_generator
+	DevViewTool::Renderer.copyright 'Jordan M', 'All rights reserved'
+	end
 end
