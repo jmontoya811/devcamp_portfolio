7df2e99b86befa8f2d116760761232a31abbf0a5
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-10-25 11:40:49 -0600

    implemented petergate and authorizations for site admin

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,7 @@
 class PortfoliosController < ApplicationController
-  
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy,]
   layout "portfolios"
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
 
   def index
     @portfolio_items = Portfolio.all
@@ -67,6 +68,10 @@ class PortfoliosController < ApplicationController
   end
 
     private
+
+    def set_portfolio
+      @portfolio = Portfolio.find(params[:id])
+    end
 
     def portfolio_params
       params.require(:portfolio).permit(:title, 
