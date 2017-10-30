ba611f6347fbcd1ee500e31556ceae51d26a2c00
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-10-30 14:02:00 -0600

    implement portfolio layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,25 +1,59 @@
 module ApplicationHelper
 	def login_helper style = ''
-		 if current_user.is_a?(GuestUser) 
-		 	(link_to "Register", new_user_registration_path, class: style) +
-       		" ".html_safe +
-       		(link_to "Login", new_user_session_path, class: style)
-			 
-         else 
-       		link_to "Logout", destroy_user_session_path, method: :delete, class: style
-        
-        end 
+		if current_user.is_a?(GuestUser) 
+			(link_to "Register", new_user_registration_path, class: style) +
+			" ".html_safe +
+			(link_to "Login", new_user_session_path, class: style)
+
+		else 
+			link_to "Logout", destroy_user_session_path, method: :delete, class: style
+
+		end 
 	end
 
 
 	def source_helper(layout_name)
 		if session[:source] 
-		greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
-		content_tag(:p, greeting, class: "source_greeting")
+			greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+			content_tag(:p, greeting, class: "source_greeting")
+		end
 	end
-end
 
-def copyright_generator
-	DevViewTool::Renderer.copyright '&copy; Jordan M', 'All rights reserved'
+	def copyright_generator
+		DevViewTool::Renderer.copyright '&copy; Jordan M', 'All rights reserved'
+	end
+
+	def nav_items
+		[ 
+			{
+				url: root_path,
+				title: 'Home'
+				},{
+				url: blogs_path,
+				title: 'Blog'
+				},{
+				url: portfolios_path,
+				title: 'Portfolio'
+				},{
+				url: about_path,
+				title: 'About'
+				},{
+				url: contact_path,
+				title: 'Contact'
+				}
+			]
+	end
+
+	def nav_helper style, tag_type
+		nav_links = ''
+		nav_items.each do |item|
+			nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+	end
+
+	  nav_links.html_safe
+	end
+
+	def active? path
+		"active" if current_page? path
 	end
 end
