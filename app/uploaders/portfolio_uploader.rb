bb58bcfd9018a4bec9514eb6e1f7a26a26f1d522
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-10-31 13:54:03 -0600

    implemented initial file uploading system via carrierwave for portfolio

---
+++ app/uploaders/portfolio_uploader.rb
@@ -0,0 +1,12 @@
+class PortfolioUploader < CarrierWave::Uploader::Base
+
+  storage :file
+  
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extention_whitelist
+    %w(jpg jpeg git png)
+  end 
+end
