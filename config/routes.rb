cd7b369a452f9d518c9f45a80d1a7210f27b0d80
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-11-03 13:35:31 -0600

    created comment broadcast, configured route, updated comment model w/callback

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ end
   	end
   end
 
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 end
