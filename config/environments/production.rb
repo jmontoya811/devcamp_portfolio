b1c82141ae00825e6cd5ff51c70c636c12632078
jmontoya811 <32622895+jmontoya811@users.noreply.github.com>
2017-11-07 22:33:09 -0700

    implemented actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://myspecialportfolio.herokuapp.com', 'http://myspecialportfolio.herokuapp.com']
+config.action_cable.url = "wss://myspecialportfolio.herokuapp.com/cable"
 end
